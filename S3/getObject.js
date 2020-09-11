25693a32014cc589599a94355358a836cdb6f757
coffeetea99 <coffeetea99iro@gmail.com>
2020-09-11 15:06:43 +0900

    S3.getObject async/await 버전 추가

--- S3/getObject.js
+++ S3/getObject.js
@@ -10,5 +10,18 @@ const getParams = {
 }
 
 S3.getObject(getParams, (err, data) => {
+    console.log('callback');
     console.log(data.Body.toString());
 })
+
+async function main() {
+    const p = await S3.getObject(getParams).promise();
+    console.log('async/await');
+    console.log(p.Body.toString());
+}
+
+main();
+
+/*
+callback과 async/await의 순서는 언제든지 바꾸어 나올 수 있다.
+*/
